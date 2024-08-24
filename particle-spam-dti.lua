ed571b67362bbc01d704a9eb5e830e26bb3daa40
hellohellohell012321 <hhellohellohell012321@gmail.com>
2024-08-23 18:04:05 -0700

    Create particle-spam-dti.lua

---
+++ particle-spam-dti.lua
@@ -0,0 +1,94 @@
+while true do
+
+
+local args = {
+    [1] = 0
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Judge"):WaitForChild("RemoteEvent"):FireServer(unpack(args))
+
+game:GetService("ReplicatedStorage"):WaitForChild("Judge"):WaitForChild("RemoteEvent"):FireServer()
+
+local args = {
+    [1] = 1
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Judge"):WaitForChild("RemoteEvent"):FireServer(unpack(args))
+
+game:GetService("ReplicatedStorage"):WaitForChild("Judge"):WaitForChild("RemoteEvent"):FireServer()
+
+local args = {
+    [1] = 2
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Judge"):WaitForChild("RemoteEvent"):FireServer(unpack(args))
+
+game:GetService("ReplicatedStorage"):WaitForChild("Judge"):WaitForChild("RemoteEvent"):FireServer()
+
+local args = {
+    [1] = 3
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Judge"):WaitForChild("RemoteEvent"):FireServer(unpack(args))
+
+game:GetService("ReplicatedStorage"):WaitForChild("Judge"):WaitForChild("RemoteEvent"):FireServer()
+
+local args = {
+    [1] = 4
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Judge"):WaitForChild("RemoteEvent"):FireServer(unpack(args))
+
+game:GetService("ReplicatedStorage"):WaitForChild("Judge"):WaitForChild("RemoteEvent"):FireServer()
+
+local args = {
+    [1] = 5
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Judge"):WaitForChild("RemoteEvent"):FireServer(unpack(args))
+
+game:GetService("ReplicatedStorage"):WaitForChild("Judge"):WaitForChild("RemoteEvent"):FireServer()
+
+local args = {
+    [1] = 6
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Judge"):WaitForChild("RemoteEvent"):FireServer(unpack(args))
+
+game:GetService("ReplicatedStorage"):WaitForChild("Judge"):WaitForChild("RemoteEvent"):FireServer()
+
+local args = {
+    [1] = 7
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Judge"):WaitForChild("RemoteEvent"):FireServer(unpack(args))
+
+game:GetService("ReplicatedStorage"):WaitForChild("Judge"):WaitForChild("RemoteEvent"):FireServer()
+
+local args = {
+    [1] = 8
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Judge"):WaitForChild("RemoteEvent"):FireServer(unpack(args))
+
+game:GetService("ReplicatedStorage"):WaitForChild("Judge"):WaitForChild("RemoteEvent"):FireServer()
+
+local args = {
+    [1] = 9
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Judge"):WaitForChild("RemoteEvent"):FireServer(unpack(args))
+
+game:GetService("ReplicatedStorage"):WaitForChild("Judge"):WaitForChild("RemoteEvent"):FireServer()
+
+local args = {
+    [1] = 10
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Judge"):WaitForChild("RemoteEvent"):FireServer(unpack(args))
+
+game:GetService("ReplicatedStorage"):WaitForChild("Judge"):WaitForChild("RemoteEvent"):FireServer()
+
+wait(0)
+
+end
